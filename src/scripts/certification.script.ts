14b8c14775609b342acd4e8fa24b3dc30d0f8bac
Juan Cruz Méndez <bolthorn91@gmail.com>
2020-10-15 20:09:05 +0100

    createPdfs

--- src/scripts/certification.script.ts
+++ src/scripts/certification.script.ts
@@ -1,22 +1,35 @@
 import csvParser from 'csv-parser';
 import fs from 'fs'
+import * as html2pdf from 'html-pdf';
+interface usersWithPdf {
+    user: string;
+    license: string;
+    email: string;
+    pdfPath: string;
+}
 
 class CertificationScript {
     constructor() {}
 
-    public initializeCertification() {
-        const filesData = this.getCSVs();
+    public async initializeCertification() {
+        const filesData = await this.getCSVs();
+        console.log('%j', filesData)
+        const users = filesData.reduce((acc: any[], current: any) => [...acc, ...current], [])
+        const usersWithPdfPath: usersWithPdf[] = await Promise.all(users.map(user => ({...user, pdf: this.getLicensePdfPath(user.name)})));
     }
 
 
     private async getCSVs(){
-        console.log(__dirname)
         const directory: string[] | [] = fs.readdirSync(`${__dirname}/../../uploads`)
         const filesDataPromises = directory.map(file => {
             return new Promise((resolve, reject) => {
                 let data: any = [];
                 fs.createReadStream(`${__dirname}/../../uploads/${file}`)
-                .pipe(csvParser())
+                // @ts-ignore
+                .pipe(csvParser({ separator: ';', from_line: 2 }))
+                .on('headers', (headers) => {
+                    console.log(`First header: ${headers[0]}`)
+                  })
                 .on('data', (row) => data.push(row))
                 .on('end', () => {
                     console.log('csv processed')
@@ -24,14 +37,38 @@ class CertificationScript {
                 });
             }) ;
         })
-        return await Promise.all(filesDataPromises);
-        
+        return (await Promise.all(filesDataPromises));
     }
 
-    private createPdf() {
+    private getLicensePdfPath(user: any): Promise<string> {
+        const content = `
+        <!doctype html>
+            <html>
+            <head>
+                    <meta charset="utf-8">
+                    <title>License</title>
+                    <style>
+                        h1 {
+                            color: green;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>Here is your license, good job!</h1>
+                </body>
+            </html>
+        `;
 
+        return new Promise((resolve) => {
+            html2pdf.create(content).toFile(`${__dirname}/../../pdfs/${user}_${Date.now()}.pdf`, (err, res) => {
+                if (err){
+                        console.log(err);
+                    }
+                resolve('test')
+                console.log({res});
+            });
+        }) 
     }
-
 }
 
 export default new CertificationScript();
