900bbbb69a63fcb2dd1ec85ac30bbd9802c8228a
Juan Cruz Méndez <bolthorn91@gmail.com>
2020-10-15 16:52:46 +0100

    serverUp

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+import cors from 'cors';
+
+const { BASE_URL, MONGO_URL, DB_PASSWORD, DB_USER, MONGO_DB_NAME, MODE } = process.env;
+const app = express();
+mongoose.connect(`${BASE_URL}${DB_USER}:${encodeURI(DB_PASSWORD as string)}${MONGO_URL}/${MONGO_DB_NAME}`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors())
+
+const port = process.env.PORT || 80;
+app.listen(port, () => console.log(`Running at http://localhost:${port}`))
